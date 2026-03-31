6257586cebd3e161f1db726cba3936c30577a788
moldovanolimpiu <moldovan.vlad22@gmail.com>
2026-04-01 00:13:10 +0300

    src/main/java/fse: add file metadata preview
    Once a file in the result list is clicked, all its info is displayed,
    and in terms of content, the first 5 lines are shown, if there are that
    many

--- src/main/java/fse/ApplicationGUI.java
+++ src/main/java/fse/ApplicationGUI.java
@@ -31,8 +31,43 @@ public class ApplicationGUI extends Application {
         searchBar.setMaxWidth(400);
         searchBar.setPromptText("Search filenames...");
 
+        Label fileNameLabel = new Label("");
+        Label filePathLabel = new Label("");
+        Label fileExtensionLabel = new Label("");
+        Label fileSizeLabel = new Label("");
+        Label fileHashLabel = new Label("");
+        Label fileContentLabel = new Label("");
+        fileContentLabel.setMaxWidth(700);
+        fileContentLabel.setWrapText(true);
+        Label fileDateCreatedLabel = new Label("");
+        Label fileDateModifiedLabel = new Label("");
+
+        VBox resultBox = new VBox(10);
+        resultBox.setAlignment(Pos.CENTER_LEFT);
+        resultBox.getChildren().addAll(fileNameLabel, filePathLabel,fileExtensionLabel,fileSizeLabel,fileHashLabel,fileDateCreatedLabel,fileDateModifiedLabel,fileContentLabel);
+
+
+
+
         VBox resultsContainer = new VBox(5);
         ToggleGroup selectionGroup = new ToggleGroup();
+
+        selectionGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue!=null){
+                FileMetadata data = (FileMetadata) newValue.getUserData();
+
+                fileNameLabel.setText("File name: " + data.getFileName());
+                filePathLabel.setText("Path: " + data.getPath());
+                fileExtensionLabel.setText("Extension: " + data.getFileExtension());
+                fileSizeLabel.setText("Size: " + sizeFormat(data.getFileSize()));
+                fileHashLabel.setText("Hash: " + data.getHash());
+                fileDateCreatedLabel.setText("Date created: " + data.getDatecreated());
+                fileDateModifiedLabel.setText("Date modified: " + data.getDatemodified());
+                fileContentLabel.setText("Content:\n " + contentPreview(data.getContent()));
+
+
+            }
+        });
 
 
 
@@ -52,10 +87,11 @@ public class ApplicationGUI extends Application {
                 resultsContainer.getChildren().clear();
 
                 for(FileMetadata file : results) {
-                    ToggleButton tb = new ToggleButton(file.getFileName());
+                    ToggleButton tb = new ToggleButton(file.getPath());
                     tb.setToggleGroup(selectionGroup);
                     tb.setMaxWidth(700);
                     tb.getStyleClass().add("label-button");
+                    tb.setUserData(file);
 
                     resultsContainer.getChildren().add(tb);
                 }
@@ -69,12 +105,48 @@ public class ApplicationGUI extends Application {
         scrollPane.setStyle("-fx-background-color: transparent;");
         scrollPane.setMaxHeight(200);
 
-        mainLayout.getChildren().addAll(searchBar,scrollPane);
+        mainLayout.getChildren().addAll(searchBar,scrollPane, resultBox);
 
         Scene mainScene = new Scene(mainLayout, 700, 800);
         mainScene.getStylesheets().add(getClass().getResource("/fse/style.css").toExternalForm());
 
         primaryStage.setScene(mainScene);
         primaryStage.show();
+    }
+
+    private String sizeFormat(long size) {
+
+        //long var = 0;
+        String appendage;
+        int count = 0;
+        while(size > 1023){
+            size =size/1024;
+            count++;
+        }
+        if(count == 0){
+            appendage = size + " B";
+        }else if(count == 1){
+            appendage = size + " KB";
+        }else if(count == 2){
+            appendage = size + " MB";
+        }else if(count == 3){
+            appendage = size + " GB";
+        }else{
+            appendage = size + " TB";
+        }
+        return appendage;
+
+    }
+
+    private StringBuilder contentPreview(String content){
+        StringBuilder sb = new StringBuilder();
+        String[] contentArray = content.split("\n");
+        int size = Math.min(contentArray.length, 5);
+        for(int i = 0; i < size; i++){
+            sb.append(contentArray[i]);
+            sb.append("\n");
+        }
+
+        return sb;
     }
 }
