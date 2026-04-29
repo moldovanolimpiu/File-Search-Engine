b245d51658589efb962077aba87dfb199339ae2d
moldovanolimpiu <moldovan.vlad22@gmail.com>
2026-04-29 22:19:40 +0300

    QueryData: add new class QueryData
    Added a class which is used to organize the data obtained from processing
    the query, which is further sent to the search function that uses it to
    find the needed files

---
+++ src/main/java/fse/QueryData.java
@@ -0,0 +1,21 @@
+package fse;
+
+import java.util.List;
+
+public class QueryData {
+    private String path;
+    private String content;
+
+    public QueryData(String path, String content) {
+        this.path = path;
+        this.content = content;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
